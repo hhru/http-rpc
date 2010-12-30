b87deb1b8a77a18010afbec3638ab6f7adb627ca
Anton Lebedevich <mabrek@gmail.com>
2010-12-30 15:02:46 +0300

    client call test

--- src/test/java/ru/hh/search/httprpc/RequestReplyTest.java
+++ src/test/java/ru/hh/search/httprpc/RequestReplyTest.java
@@ -3,22 +3,26 @@ package ru.hh.search.httprpc;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.hh.search.httprpc.client.NettyClient;
 import ru.hh.search.httprpc.server.NettyServer;
+import static org.testng.Assert.assertEquals;
 
 public class RequestReplyTest {
   private NettyServer server;
   private NettyClient client;
-  
+  private HelloMethod method;
+
   @BeforeClass
   public void setUp() {
     Map<String, Object> serverOptions = new HashMap<String, Object>();
     serverOptions.put("localAddress", new InetSocketAddress(12345));
     server = new NettyServer(serverOptions);
-    server.register(new HelloMethod());
+    method = new HelloMethod();
+    server.register(method);
     server.startAndWait();
     
     Map<String, Object> clientOptions = new HashMap<String, Object>();
@@ -28,7 +32,9 @@ public class RequestReplyTest {
   }
   
   @Test
-  public void test() {
+  public void test() throws ExecutionException, InterruptedException {
+    String argument = "hello!";
+    assertEquals(client.call(method.getUri(), null, argument).get(), method.call(null, argument));
   }
   
   @AfterClass
