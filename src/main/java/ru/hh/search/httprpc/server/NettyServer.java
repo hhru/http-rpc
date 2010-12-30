a3c9536d79901256c352ff8713f95e096fde8387
Anton Lebedevich <mabrek@gmail.com>
2010-12-30 16:14:21 +0300

    server handler skeleton

--- src/main/java/ru/hh/search/httprpc/server/NettyServer.java
+++ src/main/java/ru/hh/search/httprpc/server/NettyServer.java
@@ -6,14 +6,32 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executors;
 import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelFutureListener;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.channel.group.ChannelGroup;
 import org.jboss.netty.channel.group.DefaultChannelGroup;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
+import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
+import org.jboss.netty.handler.codec.http.HttpRequest;
+import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
+import org.jboss.netty.handler.codec.http.HttpResponse;
+import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
+import org.jboss.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.hh.search.httprpc.ServerMethod;
+import static org.jboss.netty.channel.Channels.pipeline;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.OK;
+import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class NettyServer extends AbstractService {
   
@@ -33,7 +51,33 @@ public class NettyServer extends AbstractService {
       Executors.newCachedThreadPool());
     bootstrap = new ServerBootstrap(factory);
     bootstrap.setOptions(options);
-    // TODO pipeline factory
+    bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+      public ChannelPipeline getPipeline() throws Exception {
+        ChannelPipeline pipeline = pipeline();
+        pipeline.addLast("decoder", new HttpRequestDecoder());
+        pipeline.addLast("aggregator", new HttpChunkAggregator(Integer.MAX_VALUE));
+        pipeline.addLast("encoder", new HttpResponseEncoder());
+        pipeline.addLast("handler", new RequestHandler());
+        return pipeline;
+      }
+    });
+  }
+  
+  private class RequestHandler extends SimpleChannelUpstreamHandler {
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+      HttpRequest request = (HttpRequest) e.getMessage();
+      // TODO: no method??
+      // TODO: parse parameters
+      ServerMethod method = methods.get(request.getUri());
+      // TODO: unserialize request
+      // TODO: method types
+      String result = ((ServerMethod<String, String>)method).call(null, request.getContent().toString(CharsetUtil.UTF_8));
+      HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+      response.setContent(ChannelBuffers.copiedBuffer(result, CharsetUtil.UTF_8));
+      response.setHeader(CONTENT_TYPE, "text/plain; charset=UTF-8");
+      e.getChannel().write(response).addListener(ChannelFutureListener.CLOSE);
+    }
   }
 
   @Override
