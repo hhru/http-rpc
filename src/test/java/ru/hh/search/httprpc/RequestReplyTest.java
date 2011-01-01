cc98bf1b3c6abaa55874dda6b844ff44f12206c6
Anton Lebedevich <mabrek@gmail.com>
2011-01-01 18:49:42 +0300

    test converted to use parameters

--- src/test/java/ru/hh/search/httprpc/RequestReplyTest.java
+++ src/test/java/ru/hh/search/httprpc/RequestReplyTest.java
@@ -4,43 +4,36 @@ import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.hh.search.httprpc.client.NettyClient;
 import ru.hh.search.httprpc.server.NettyServer;
 import static org.testng.Assert.assertEquals;
 
 public class RequestReplyTest {
-  private NettyServer server;
-  private NettyClient client;
-  private HelloMethod method;
 
-  @BeforeClass
-  public void setUp() {
-    Serializer serializer = new JavaSerializer();
-    
+  @DataProvider(name = "methods")
+  public Object[][] methods() {
+    return new Object[][] {
+      {new JavaSerializer(), new HelloMethod(), "hello"}
+    };
+  }
+  
+  @Test(dataProvider = "methods")
+  public void test(Serializer serializer, ServerMethod method, Object argument) throws ExecutionException, InterruptedException {
     Map<String, Object> serverOptions = new HashMap<String, Object>();
     serverOptions.put("localAddress", new InetSocketAddress(12345));
-    server = new NettyServer(serverOptions, serializer);
-    method = new HelloMethod();
+    NettyServer server = new NettyServer(serverOptions, serializer);
     server.register(method);
     server.startAndWait();
-    
+
     Map<String, Object> clientOptions = new HashMap<String, Object>();
     clientOptions.put("remoteAddress", new InetSocketAddress(12345));
-    client = new NettyClient(clientOptions, serializer);
+    NettyClient client = new NettyClient(clientOptions, serializer);
     client.startAndWait();
-  }
-  
-  @Test
-  public void test() throws ExecutionException, InterruptedException {
-    String argument = "hello!";
+
     assertEquals(client.call(method.getPath(), null, argument, String.class).get(), method.call(null, argument));
-  }
-  
-  @AfterClass
-  public void tearDown() {
+
     client.stopAndWait();
     server.stopAndWait();
   }
