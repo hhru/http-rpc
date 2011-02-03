f2b0f34c9d935f53ece75509879540d22f70b915
Anton Lebedevich <mabrek@gmail.com>
2011-02-03 22:16:25 +0300

    cosmetics

--- src/test/java/ru/hh/search/httprpc/ManyLongTasksTest.java
+++ src/test/java/ru/hh/search/httprpc/ManyLongTasksTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class ManyLongTasksTest extends AbstractClientServerTest {
   @Test
@@ -19,11 +20,14 @@ public class ManyLongTasksTest extends AbstractClientServerTest {
     
     Envelope envelope = new Envelope(10, "qwerty");
     
+    // assume that we have more callThreads than number of slow tasks
+    assertTrue(callThreads > ioThreads + 1);
+    
     // flood server's ioThreads with long tasks (if it processes them in ioThreads) 
     for (int i = 0; i < ioThreads + 1; i++) {
-      clientMethod.call(address, envelope, new Long(10000));
+      clientMethod.call(address, envelope, 10000L);
     }
     
-    assertEquals(clientMethod.call(address, envelope, 1).get(1, TimeUnit.SECONDS), 1);
+    assertEquals(clientMethod.call(address, envelope, 1L).get(1, TimeUnit.SECONDS), 1L);
   }
 }
