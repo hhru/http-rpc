53f939122ce12a9ba795d14214c400ef25f9af33
Anton Lebedevich <mabrek@gmail.com>
2011-02-05 12:33:09 +0300

    switches test to common AbstractClientServerTest base

--- src/test/java/ru/hh/search/httprpc/RequestReplyTest.java
+++ src/test/java/ru/hh/search/httprpc/RequestReplyTest.java
@@ -1,17 +1,11 @@
 package ru.hh.search.httprpc;
 
-import com.google.common.util.concurrent.MoreExecutors;
-import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import ru.hh.search.httprpc.netty.NettyClient;
-import ru.hh.search.httprpc.netty.NettyServer;
 import static org.testng.Assert.assertEquals;
 
-public class RequestReplyTest {
+public class RequestReplyTest extends AbstractClientServerTest {
 
   @DataProvider(name = "methods")
   public Object[][] methods() {
@@ -26,27 +20,15 @@ public class RequestReplyTest {
   
   @Test(dataProvider = "methods")
   public void test(Serializer inputSerializer, Serializer outputSerializer, ServerMethod serverMethod, Object argument) throws ExecutionException, InterruptedException {
-    // TODO switch to AbstractClientServerTest
-    InetSocketAddress address = new InetSocketAddress(12345);
-    String basePath = null;
     String path = "/helloMethod";
-    
-    Map<String, Object> serverOptions = new HashMap<String, Object>();
-    serverOptions.put("localAddress", address);
-    NettyServer server = new NettyServer(serverOptions, basePath, 2, MoreExecutors.sameThreadExecutor());
     server.register(path, serverMethod, outputSerializer, inputSerializer);
-    server.startAndWait();
 
     Object local = serverMethod.call(null, argument);
     
-    NettyClient client = new NettyClient(new HashMap<String, Object>(), basePath, 2);
     ClientMethod clientMethod = client.createMethod(path, inputSerializer, outputSerializer);
     Object remote = clientMethod.call(address, new Envelope(10, "asdf"), argument).get();
     
     assertEquals(remote, local);
-
-    client.stopAndWait();
-    server.stopAndWait();
   }
 
   public static class JavaMethod implements ServerMethod<String, String> {
