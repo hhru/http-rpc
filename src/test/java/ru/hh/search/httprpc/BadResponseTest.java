31486e5c64964e081fcffd609ad08dd03946d16d
Anton Lebedevich <mabrek@gmail.com>
2011-02-05 12:27:02 +0300

    bad response test implemented

---
+++ src/test/java/ru/hh/search/httprpc/BadResponseTest.java
@@ -0,0 +1,39 @@
+package ru.hh.search.httprpc;
+
+import java.util.concurrent.ExecutionException;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+public class BadResponseTest extends AbstractClientServerTest {
+  @Test
+  public void test() throws InterruptedException {
+    Serializer<Object> serializer = new JavaSerializer<Object>();
+    String path = "/throwMethod";
+    server.register(path, new ThrowMethod(), serializer, serializer);
+
+    ClientMethod<Object, String> clientMethod = client.<Object, String>createMethod(path, serializer, serializer);
+
+    String message = "message to be returned as exception";
+    try {
+      clientMethod.call(address, new Envelope(123, "123"), message).get();
+      fail();
+    } catch (ExecutionException e) {
+      if (e.getCause() instanceof BadResponseException) {
+        BadResponseException cause = (BadResponseException) e.getCause();
+        assertTrue(cause.getMessage().contains("500"));
+        assertTrue(cause.getDetails().contains(message));
+      } else {
+        fail("unexpected cause", e.getCause());
+      }
+    }
+
+  }
+  
+  private static class ThrowMethod implements ServerMethod<Object, String> {
+    @Override
+    public Void call(Envelope envelope, String message) {
+      throw new RuntimeException(message);
+    }
+  }
+}
