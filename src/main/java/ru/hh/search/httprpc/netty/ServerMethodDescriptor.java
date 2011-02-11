3951e592b8ef35fd4a30c6b80a2b6f5b2a97d5c7
Anton Lebedevich <mabrek@gmail.com>
2011-02-11 15:46:57 +0300

    generic method descriptor

--- src/main/java/ru/hh/search/httprpc/netty/ServerMethodDescriptor.java
+++ src/main/java/ru/hh/search/httprpc/netty/ServerMethodDescriptor.java
@@ -3,12 +3,12 @@ package ru.hh.search.httprpc.netty;
 import ru.hh.search.httprpc.Serializer;
 import ru.hh.search.httprpc.ServerMethod;
 
-class ServerMethodDescriptor {
-  final public ServerMethod method;
-  final public Serializer encoder;
-  final public Serializer decoder;
+class ServerMethodDescriptor<I, O> {
+  final public ServerMethod<I, O> method;
+  final public Serializer<O> encoder;
+  final public Serializer<I> decoder;
 
-  ServerMethodDescriptor(ServerMethod method, Serializer encoder, Serializer decoder) {
+  ServerMethodDescriptor(ServerMethod<I, O> method, Serializer<O> encoder, Serializer<I> decoder) {
     this.method = method;
     this.encoder = encoder;
     this.decoder = decoder;
