888ffcd36013ed3c24d124104b75e47707abc576
Earwin Burrfoot <earwin@gmail.com>
2011-02-12 18:22:08 +0300

    faster java deserialization (avoids Class.forName, which tends to go through a heap of classloaders and is synchronized, as of 1.6)

--- src/main/java/ru/hh/search/httprpc/JavaSerializerFactory.java
+++ src/main/java/ru/hh/search/httprpc/JavaSerializerFactory.java
@@ -1,7 +1,14 @@
 package ru.hh.search.httprpc;
 
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.MapMaker;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
+import java.util.concurrent.ConcurrentMap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBufferInputStream;
 import org.jboss.netty.buffer.ChannelBufferOutputStream;
@@ -34,10 +41,33 @@ public class JavaSerializerFactory implements SerializerFactory {
     @SuppressWarnings("unchecked")
     public T deserialize(ChannelBuffer serialForm) throws SerializationException {
       try {
-        ObjectInputStream ois = new ObjectInputStream(new ChannelBufferInputStream(serialForm));
+        ObjectInputStream ois = new FastObjectInputStream(new ChannelBufferInputStream(serialForm));
         return (T) ois.readObject();
       } catch (Exception e) {
         throw new SerializationException(e);
+      }
+    }
+
+    // Todo: Use JBoss Marshalling/Serialization?
+    private static class FastObjectInputStream extends ObjectInputStream {
+      private static final ConcurrentMap<String, Class<?>> CLASS_CACHE = new MapMaker().
+          concurrencyLevel(Runtime.getRuntime().availableProcessors()).
+          makeComputingMap(new Function<String, Class<?>>() {
+            public Class<?> apply(String input) {
+              try {
+                return Class.forName(input);
+              } catch (ClassNotFoundException e) {
+                throw Throwables.propagate(e);
+              }
+            }
+          });
+
+      public FastObjectInputStream(InputStream in) throws IOException {
+        super(in);
+      }
+
+      protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
+        return CLASS_CACHE.get(desc.getName());
       }
     }
   }
